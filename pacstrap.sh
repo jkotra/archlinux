76b39d1b4ca267376a2350beb4d832f4c94aab30
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-05-03 21:40:31 +0530

    fix fstab

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pacstrap /mnt base base-devel linux-lts linux-firmware python
-genfstab -U /mnt >> /mnt/etc/fstab
 echo "swap partition:"
 read swap
 swapon $swap
 cp -R archlinux /mnt
+genfstab -U /mnt >> /mnt/etc/fstab
