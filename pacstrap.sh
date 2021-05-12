6915d3acf7c538b2187ffb9e9ad865b11720369b
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-05-12 13:27:44 +0530

    add linux-lts-headers

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacstrap /mnt base base-devel linux-lts linux-firmware python
+pacstrap /mnt base base-devel linux-lts linux-firmware linux-lts-headers python
 echo "swap partition:"
 read swap
 swapon $swap
